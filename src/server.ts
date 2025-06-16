72a79f87503551e4bea701b11e1b6f0c54a5d14a
DESKTOP-EJEGLOR\MEA <graywolf7235@gmail.com>
2025-06-16 18:06:50 +0800

    package.json修改start

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,10 @@
-import app from './app';
-import dotenv from 'dotenv';
+import app from "./app";
+import dotenv from "dotenv";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
 
-
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
