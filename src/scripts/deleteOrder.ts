ec579e2af8c56284f09a01a5aef58b7df7c3195a
DESKTOP-EJEGLOR\MEA <graywolf7235@gmail.com>
2025-06-20 12:42:32 +0800

    修改notice.ts20

--- src/scripts/deleteOrder.ts
+++ src/scripts/deleteOrder.ts
@@ -6,7 +6,7 @@ async function cleanup() {
 
   const result = await prisma.order.deleteMany({
     where: {
-      userId: "9da192f5-1683-4fdc-acec-157f0a89c233",
+      userId: "157cb968-b516-4c87-a315-c809b2f01b56",
     },
   });
 
