3c00ff663eeeed16d0a31f2b4541ecaa7dbb818f
DESKTOP-EJEGLOR\MEA <graywolf7235@gmail.com>
2025-06-20 14:14:27 +0800

    修改notice.ts27

--- src/routes/notice.ts
+++ src/routes/notice.ts
@@ -14,24 +14,27 @@ if (!HASH_KEY || !HASH_IV) {
 }
 console.log("HASH_KEY:", HASH_KEY, "length:", HASH_KEY.length);
 console.log("HASH_IV:", HASH_IV, "length:", HASH_IV.length);
-const verifyTradeSha = (tradeInfo: string, tradeSha: string) => {
-  const raw = `HashKey=${HASH_KEY}&${tradeInfo}&HashIV=${HASH_IV}`;
-  const calculatedSha = crypto
-    .createHash("sha256")
-    .update(raw)
-    .digest("hex")
-    .toUpperCase();
-  return calculatedSha === tradeSha;
-};
 
+// const aesDecrypt = (encryptedText: string) => {
+//   if (!HASH_KEY || !HASH_IV) throw new Error("缺少金鑰設定");
+//   const key = Buffer.from(HASH_KEY, "utf8");
+//   const iv = Buffer.from(HASH_IV, "utf8");
+//   const decipher = crypto.createDecipheriv("aes-256-cbc", key, iv);
+//   let decrypted = decipher.update(encryptedText, "hex", "utf8");
+//   decrypted += decipher.final("utf8");
+//   return qs.parse(decrypted);
+// };
 const aesDecrypt = (encryptedText: string) => {
   if (!HASH_KEY || !HASH_IV) throw new Error("缺少金鑰設定");
   const key = Buffer.from(HASH_KEY, "utf8");
   const iv = Buffer.from(HASH_IV, "utf8");
   const decipher = crypto.createDecipheriv("aes-256-cbc", key, iv);
-  let decrypted = decipher.update(encryptedText, "hex", "utf8");
-  decrypted += decipher.final("utf8");
-  return qs.parse(decrypted);
+  let decrypted = Buffer.concat([
+    decipher.update(encryptedText, "hex"),
+    decipher.final(),
+  ]);
+
+  return qs.parse(decrypted.toString("utf8"));
 };
 
 router.post("/", async (req: Request, res: Response) => {
